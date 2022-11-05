a1077306a7f07786242a6b40f6ba08d754d845b0
frozenpine <sonny.frozenpine@gmail.com>
2022-11-05 13:55:53 +0800

    初步完成channel

--- hub.go
+++ hub.go
@@ -8,30 +8,43 @@ import (
 	"time"
 )
 
+var (
+	ErrNoSubcriber = errors.New("no subscriber")
+	ErrHubClosed   = errors.New("hub closed")
+	ErrPubTimeout  = errors.New("pub timeout")
+
+	defaultChanLen = 10
+)
+
 type Hub[T any] struct {
-	init     sync.Once
-	release  sync.Once
+	init    sync.Once
+	release sync.Once
+
 	runCtx   context.Context
 	cancelFn context.CancelFunc
 
-	timeout time.Duration
+	chanLen int
 
 	subCache sync.Map
-	subLen   int
+	subWg    sync.WaitGroup
 
 	pubCache sync.Map
-	pubLen   int
 }
 
-func NewHub[T any](ctx context.Context, bufSize int, timeout time.Duration) *Hub[T] {
+// NewHub create channel hub.
+//
+// bufSize: pub & sub chan buffer size, if bufSize < 0, use default size 10
+func NewHub[T any](ctx context.Context, bufSize int) *Hub[T] {
 	if ctx == nil {
 		ctx = context.Background()
 	}
 
+	if bufSize < 0 {
+		bufSize = defaultChanLen
+	}
+
 	hub := Hub[T]{
-		timeout: timeout,
-		subLen:  bufSize,
-		pubLen:  2 * bufSize,
+		chanLen: bufSize,
 	}
 
 	hub.init.Do(func() {
@@ -42,14 +55,32 @@ func NewHub[T any](ctx context.Context, bufSize int, timeout time.Duration) *Hub
 }
 
 func (hub *Hub[T]) Stop() {
-	hub.release.Do(func() { hub.cancelFn() })
+	hub.release.Do(func() {
+		hub.cancelFn()
+
+		hub.pubCache.Range(func(topic, topicPub any) bool {
+			pubCh := topicPub.(chan T)
+
+			log.Printf("Closing pub channel for topic: %s", topic.(string))
+
+			close(pubCh)
+
+			return true
+		})
+	})
 }
 
 func (hub *Hub[T]) Join() {
 	<-hub.runCtx.Done()
+
+	hub.subWg.Wait()
 }
 
 func (hub *Hub[T]) dispatcher(topic string, pubCh <-chan T) {
+	hub.subWg.Add(1)
+
+	defer hub.subWg.Done()
+
 	for {
 		topicSub, exist := hub.subCache.Load(topic)
 
@@ -64,6 +95,7 @@ func (hub *Hub[T]) dispatcher(topic string, pubCh <-chan T) {
 			subCache.Range(func(subcriber, subCh any) bool {
 				ch := subCh.(chan T)
 
+				log.Printf("Closing sub channel for subscriber[%+v] on topic[%s].", subcriber, topic)
 				close(ch)
 
 				return true
@@ -76,7 +108,7 @@ func (hub *Hub[T]) dispatcher(topic string, pubCh <-chan T) {
 
 				select {
 				case <-time.After(time.Second):
-					log.Printf("Publish timeout to subscriber[%v] on topic[%s].", subcriber, topic)
+					log.Printf("Publish timeout to subscriber[%+v] on topic[%s].", subcriber, topic)
 				case ch <- v:
 				}
 
@@ -89,9 +121,13 @@ func (hub *Hub[T]) dispatcher(topic string, pubCh <-chan T) {
 func (hub *Hub[T]) Subscribe(topic string, subscriber interface{}) <-chan T {
 	topicSub, _ := hub.subCache.LoadOrStore(topic, &sync.Map{})
 	subCache := topicSub.(*sync.Map)
-	ch, subExist := subCache.LoadOrStore(subscriber, make(chan T))
+
+	ch, subExist := subCache.LoadOrStore(subscriber, make(chan T, 1))
+
 	if subExist {
 		log.Printf("Channel on topic[%s] exist for subscriber[%v]", topic, subscriber)
+	} else {
+		log.Printf("New subscriber[%+v] on topic[%s]", subscriber, topic)
 	}
 
 	hub.loadOrCreatePub(topic)
@@ -100,7 +136,11 @@ func (hub *Hub[T]) Subscribe(topic string, subscriber interface{}) <-chan T {
 }
 
 func (hub *Hub[T]) loadOrCreatePub(topic string) chan<- T {
-	topicPub, pubExist := hub.pubCache.LoadOrStore(topic, make(chan T, hub.pubLen))
+	if _, exist := hub.subCache.Load(topic); !exist {
+		return nil
+	}
+
+	topicPub, pubExist := hub.pubCache.LoadOrStore(topic, make(chan T, 1))
 	pubCh := topicPub.(chan T)
 
 	if !pubExist {
@@ -110,18 +150,25 @@ func (hub *Hub[T]) loadOrCreatePub(topic string) chan<- T {
 	return pubCh
 }
 
-func (hub *Hub[T]) PublishWithTimeout(topic string, v T, timeout time.Duration) error {
-	pubCh := hub.loadOrCreatePub(topic)
+func (hub *Hub[T]) timeout(timeout time.Duration) <-chan time.Time {
+	if timeout > 0 {
+		return time.After(timeout)
+	}
 
-	if timeout <= 0 {
-		timeout = hub.timeout
+	return make(<-chan time.Time)
+}
+
+func (hub *Hub[T]) Publish(topic string, v T, timeout time.Duration) error {
+	pubCh := hub.loadOrCreatePub(topic)
+	if pubCh == nil {
+		return ErrNoSubcriber
 	}
 
 	select {
 	case <-hub.runCtx.Done():
-		return errors.New("hub closed")
-	case <-time.After(timeout):
-		return errors.New("pub timeout")
+		return ErrHubClosed
+	case <-hub.timeout(timeout):
+		return ErrPubTimeout
 	case pubCh <- v:
 		return nil
 	}
--- hub_test.go
+++ hub_test.go
@@ -2,9 +2,7 @@ package channel
 
 import (
 	"context"
-	"sync"
 	"testing"
-	"time"
 )
 
 func TestHub(t *testing.T) {
@@ -12,34 +10,27 @@ func TestHub(t *testing.T) {
 	sub1 := "sub1"
 	sub2 := "sub2"
 
-	hub := NewHub[int](context.TODO(), 10, time.Duration(1)*time.Second)
-
-	wg := sync.WaitGroup{}
-	wg.Add(2)
+	hub := NewHub[int](context.TODO(), -1)
 
 	go func() {
 		for v := range hub.Subscribe(topic, sub1) {
 			t.Log(sub1, v)
 		}
-
-		wg.Done()
 	}()
 
 	go func() {
 		for v := range hub.Subscribe(topic, sub2) {
 			t.Log(sub2, v)
 		}
-
-		wg.Done()
 	}()
 
 	for idx := 0; idx < 100; idx++ {
-		if err := hub.PublishWithTimeout(topic, idx, 0); err != nil {
-			t.Log(err)
+		if err := hub.Publish(topic, idx, 0); err != nil {
+			t.Log(idx, err)
 		}
 	}
 
 	hub.Stop()
 
-	wg.Wait()
+	hub.Join()
 }
